28831d2f3dd40813c61f12249d199c8e6b440941
Ian Markowich <ian.n.markowich.civ@us.navy.mil>
2024-07-08 12:57:17 -0400

    Adds pages

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Comic_Neue } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Comic_Neue({ subsets: ['latin'], weight: ['700']  });
